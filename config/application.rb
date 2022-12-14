36075bec9578473f89e6ab32467b9b8f5be408b0
Charles <charlie.david@me.com>
2022-12-14 15:19:36 -0700

    Rails Setup

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FurnishBainbridge
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+
+    config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.action_dispatch.cookies_same_site_protection = :strict
+
+    config.load_defaults 7.0
+  end
+end
